a3e26bf05a53efa8a4f2e099780962692d601025
Maria_Arsen <maria_m_84@inbox.ru>
2023-05-24 18:42:40 +0300

    Добавьте файлы проекта.

---
+++ RoomSelectionFilter.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI.Selection;
+
+namespace RevitAPIRoomNum_Total_
+{
+    public class RoomSelectionFilter : ISelectionFilter
+    {
+
+        public bool AllowElement(Autodesk.Revit.DB.Element elem)
+        {
+            if (elem is Room)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool AllowReference(Autodesk.Revit.DB.Reference reference, Autodesk.Revit.DB.XYZ position)
+        {
+            return false;
+        }
+    }
+}
